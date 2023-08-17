b9246bda868a47df95a0214888c7db849e22ec1c
Victor <reivictor.dev@gmail.com>
2023-08-17 18:03:49 -0300

    Adding paginable configuration

--- src/main/java/com/school/controllers/StudentController.java
+++ src/main/java/com/school/controllers/StudentController.java
@@ -1,8 +1,12 @@
 package com.school.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.school.data.vo.v1.StudentVO;
@@ -37,8 +42,13 @@ public class StudentController {
                         @ApiResponse(description = "Not found", responseCode = "404", content = @Content),
                         @ApiResponse(description = "Internal error", responseCode = "500", content = @Content)
         })
-        public List<StudentVO> findAll() {
-                return service.findAll();
+        public ResponseEntity<PagedModel<EntityModel<StudentVO>>> findAll(
+                        @RequestParam(value = "page", defaultValue = "0") Integer page,
+                        @RequestParam(value = "size", defaultValue = "10") Integer size,
+                        @RequestParam(value = "direction", defaultValue = "asc") String direction) {
+                var sortDirection = "desc".equalsIgnoreCase(direction) ? Direction.DESC : Direction.ASC;
+                Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection, "first_name"));
+                return ResponseEntity.ok(service.findAll(pageable));
         }
 
         @GetMapping(value = "/{id}")
--- src/main/java/com/school/services/StudentServices.java
+++ src/main/java/com/school/services/StudentServices.java
@@ -3,10 +3,14 @@ package com.school.services;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
-import java.util.List;
 import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.stereotype.Service;
 
 import com.school.controllers.StudentController;
@@ -15,7 +19,6 @@ import com.school.exceptions.RequiredObjectIsNullException;
 import com.school.exceptions.ResourceNotFoundException;
 import com.school.mapper.StudentMapper;
 import com.school.model.Student;
-import com.school.repository.GradeRepository;
 import com.school.repository.StudentRepository;
 
 @Service
@@ -26,23 +29,29 @@ public class StudentServices {
     StudentRepository repository;
 
     @Autowired
-    GradeRepository gradeRepository;
+    PagedResourcesAssembler<StudentVO> assembler;
 
     // findAll
-    public List<StudentVO> findAll() {
+    public PagedModel<EntityModel<StudentVO>> findAll(Pageable pageable) {
         logger.info("Finding all students!");
 
-        var students = StudentMapper.INSTANCE.toVOList(repository.findAll());
-        students
-                .stream()
-                .forEach(s -> {
-                    try {
-                        s.add(linkTo(methodOn(StudentController.class).findById(s.getKey())).withSelfRel());
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                });
-        return students;
+        var studentsPage = repository.findAll(pageable);
+
+        var studentsVoPage = studentsPage.map(s -> StudentMapper.INSTANCE.toVO(s));
+        studentsVoPage.map(s -> {
+            try {
+                return s.add(linkTo(methodOn(StudentController.class).findById(s.getKey())).withSelfRel());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return s;
+        });
+
+        Link link = linkTo(
+                methodOn(StudentController.class).findAll(pageable.getPageNumber(), pageable.getPageSize(), "asc"))
+                .withSelfRel();
+
+        return assembler.toModel(studentsVoPage, link);
 
     }
 
